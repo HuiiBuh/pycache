7286607b0058b3b9f0abd47f978e096d206cfb5d
HuiiBuh <nhaderer1@gmail.com>
2021-03-17 18:35:59 +0100

    Fixed tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -17,52 +17,49 @@ def time_equals(t1: datetime, t2: datetime):
 
 def test_expire_at():
     # Seconds
-    for i in range(500):
-        current = datetime.now()
+    current = datetime.now()
+    for i in range(5000):
         second = randrange(0, 59)
         time_equals(
             parse_expires_at(f"00:00:{second}"),
             datetime(current.year, current.month, current.day + 1, 0, 0, second)
         )
-    for i in range(500):
-        current = datetime.now()
+    for i in range(5000):
         second = randrange(0, 59)
         time_equals(
             parse_expires_at(f"*:00:{second}"),
             datetime(current.year, current.month, current.day, current.hour + 1, 0, second)
         )
-    for i in range(500):
-        second = randrange(0, 59)
-        current_plus = datetime.now() + timedelta(seconds=1)
-        current = datetime.now()
+    for i in range(5000):
+        second = randrange(1, 58)
+        current_plus = datetime(current.year, current.month, current.day, current.hour, current.minute, 0)
         time_equals(
             parse_expires_at(f"*:*:{second}", current_plus),
-            datetime(current.year, current.month, current.day, current.hour, current.minute + 1, second)
+            datetime(current.year, current.month, current.day, current.hour, current.minute, second)
         )
 
     # Minutes
-    for i in range(500):
-        current = datetime.now()
-        minute = randrange(0, 59)
+    for i in range(5000):
+        minute = randrange(0, 58)
         time_equals(
             parse_expires_at(f"00:{minute}:00"),
             datetime(current.year, current.month, current.day + 1, 0, minute, 0)
         )
 
-    for i in range(500):
-        current = datetime.now()
-        minute = randrange(0, 59)
+    for i in range(5000):
+        minute = randrange(0, 58)
+        current_plus = datetime(current.year, current.month, current.day, current.hour, 59, 13)
         time_equals(
-            parse_expires_at(f"*:{minute}:13"),
+            parse_expires_at(f"*:{minute}:13", current_plus),
             datetime(current.year, current.month, current.day, current.hour + 1, minute, 13)
         )
 
     # Hour
-    for i in range(500):
-        current = datetime.now()
-        hour = randrange(0, 23)
+    for i in range(5000):
+        hour = randrange(0, 22)
+        current_plus = datetime(current.year, current.month, current.day, 23, 55, 13)
         time_equals(
-            parse_expires_at(f"{hour}:55:13"),
+            parse_expires_at(f"{hour}:55:13", current_plus),
             datetime(current.year, current.month, current.day + 1, hour, 55, 13)
         )
 
